a68f2aa91016b2627d658ccf43cd123226a6d691
Ruben Rodriguez <rodriguezmalvis@gmail.com>
2018-08-19 14:27:01 -0300

    commit inicial curso vagrant

---
+++ manifests/web.pp
@@ -0,0 +1,42 @@
+package { ["openjdk-7-jre", "tomcat7", "htop", "mysql-server"]:
+    ensure => installed
+}
+
+service { "tomcat7":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["tomcat7"]    
+}
+
+service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["mysql-server"]    
+}
+
+exec { "musicjungle":
+    command => "mysqladmin -uroot create musicjungle",
+    unless => "mysql -u root musicjungle",
+    path => "/usr/bin",
+    require => Service["mysql"]
+}
+
+exec { "mysql-password" :
+    command => "mysql -uroot -e \"GRANT ALL PRIVILEGES ON * TO 'musicjungle'@'%' IDENTIFIED BY '123456';\" musicjungle",
+    unless  => "mysql -umusicjungle -pminha-senha musicjungle",
+    path => "/usr/bin",
+    require => Exec["musicjungle"]
+}
+
+file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
+    source => "/vagrant/manifests/vraptor-musicjungle.war",
+    owner => "tomcat7",
+    group => "tomcat7",
+    mode => 0644,
+    require => Package["tomcat7"],
+    notify => Service["tomcat7"]
+}
